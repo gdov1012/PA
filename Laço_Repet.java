b0f1497d269a91a73e7925f02fe0e651df515500
gdov1012 <valerogiovanna7@gmail.com>
2025-05-16 20:46:02 -0300

    Add files via upload

---
+++ Laço_Repet.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laço_repet;
+
+/**
+ *
+ * @author Admin
+ */
+public class Laço_Repet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+          for(int i=1;i<10;i++){
+         
+              if(i%2==0){
+              System.out.println("O " +i+ " eh par!");
+                      }
+             else{
+                     System.out.println("O " +i+ " eh impar!");}
+      }
+    }
+}
+    
+    
+
---
+++ Laço_Repet_2em2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laço_repet;
+
+/**
+ *
+ * @author Admin
+ */
+public class Laço_Repet_2em2 {
+   public static void main(String[] args) {
+
+     
+       int i =1;
+       while (i>0 && i<=100){
+        System.out.println("O valor de i eh:"+ i++ );
+}
+}
+}
