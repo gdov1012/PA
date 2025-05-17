ed5be2af1d105802b505ebcbc4ce11b1dce03f2e
gdov1012 <valerogiovanna7@gmail.com>
2025-05-16 22:12:30 -0300

    Add files via upload

---
+++ Laço_Repet_Dowhile.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laço_repet;
+
+/**
+ *
+ * @author Admin
+ */
+public class Laço_Repet_Dowhile {
+     public static void main(String[] args) {
+         int g = 0;
+         do{
+             System.out.println("g=" +g++);
+         } while(g<=10);
+     }
+}
